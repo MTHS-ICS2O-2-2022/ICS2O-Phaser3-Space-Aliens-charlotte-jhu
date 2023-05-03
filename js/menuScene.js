33d9f360587a19c074c8c41e277701b3133542eb
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-05-03 16:37:35 +0000

    Phaser Day 06

--- js/menuScene.js
+++ js/menuScene.js
@@ -3,27 +3,19 @@
 // Copyright (c) 2023 Charlotte Jhu All rights reserved
 //
 // Created by: Charlotte Jhu
-// Created on: April 2023
-// This is the Title Scene
+// Created on: May 2023
+// This is the Menu Scene
 
 /**
- * This class is the Title Scene.
+ * This class is the Menu Scene.
  */
-class TitleScene extends Phaser.Scene {
+class MenuScene extends Phaser.Scene {
   /**
    * This method is the constructor.
    */
   constructor() {
-    super({ key: "titleScene" })
-
-    this.titleSceneBackgroundImage = null
-    this.titleSceneText = null
-    this.titleSceneTextStyle = { 
-      font: "200px Times", 
-      fill: "#fde4b9", 
-      align: "center" 
+    super({ key: "menuScene" })
     }
-  }
 
   /**
    * Can be defined on your own Scenes.
@@ -39,9 +31,8 @@ class TitleScene extends Phaser.Scene {
    * Can be defined on your own Scenes.
    * Use it to load assets.
    */
-  preload() {
-    console.log("Title Scene")
-    this.load.image("titleSceneBackground", "./assets/aliens_screen_image.jpg")
+  preload () {
+    console.log("Menu Scene")
   }
 
   /**
@@ -50,15 +41,7 @@ class TitleScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    this.titleSceneBackgroundImage = this.add
-      .sprite(0, 0, "titleSceneBackground")
-      .setScale(2.75)
-    this.titleSceneBackgroundImage.x = 1920 / 2
-    this.titleSceneBackgroundImage.y = 1080 / 2
-
-    this.titleSceneText = this.add
-      .text(1920 / 2, 1080 / 2 + 300, "Space Aliens", this.titleSceneTextStyle)
-      .setOrigin(0.5)
+  // pass
   }
 
   /**
@@ -72,4 +55,4 @@ class TitleScene extends Phaser.Scene {
   }
 }
 
-export default TitleScene
+export default MenuScene
