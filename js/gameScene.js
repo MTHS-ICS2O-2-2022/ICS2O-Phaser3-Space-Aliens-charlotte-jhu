0c170c1b7a2ffe7a6155ac443f02fee44af5fd03
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-05-16 21:41:03 +0000

    Phaser Day 11

--- js/gameScene.js
+++ js/gameScene.js
@@ -6,13 +6,7 @@
 // Created on: May 2023
 // This is the Game Scene
 
-/**
- * This class is the Game Scene.
- */
 class GameScene extends Phaser.Scene {
-  /**
-   * This method is the constructor.
-   */
   constructor() {
     super({ key: "gameScene" })
 
@@ -21,20 +15,10 @@ class GameScene extends Phaser.Scene {
     this.fireMissile = false
     }
 
-  /**
-   * Can be defined on your own Scenes.
-   * This method is called by the Scene Manager when the scene starts,
-   *  before preload() and create().
-   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-   */
   init(data) {
     this.cameras.main.setBackgroundColor("ffffff")
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * Use it to load assets.
-   */
   preload () {
     console.log("Game Scene")
 
@@ -42,13 +26,10 @@ class GameScene extends Phaser.Scene {
     this.load.image("starBackground", "assets/starBackground.png")
     this.load.image("ship", "assets/spaceShip.png")
     this.load.image("missile", "assets/missile.png")
+    // sound
+    this.load.audio("laser", "assets/laser1.wav")
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * Use it to create your game objects.
-   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-   */
   create(data) {
   this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
   this.background.setOrigin(0, 0)
@@ -59,13 +40,9 @@ class GameScene extends Phaser.Scene {
   this.missileGroup = this.physics.add.group()
   }
 
-  /**
-   * Should be overridden by your own Scenes.
-   * This method is called once per game step while the scene is running.
-   * @param {number} time -The current time.
-   * @param {number} delta - The delta time in ms since the last frame.
-   */
   update(time, delta) {
+// called 60 times a second
+
     const keyLeftObj = this.input.keyboard.addKey("LEFT")
     const keyRightObj = this.input.keyboard.addKey("RIGHT")
     const keySpaceObj = this.input.keyboard.addKey("SPACE")
@@ -90,12 +67,20 @@ class GameScene extends Phaser.Scene {
         this.fireMissile = true
         const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, "missile")
         this.missileGroup.add(aNewMissile)
+        this.sound.play("laser")
       }
     }
 
     if (keySpaceObj.isUp === true) {
       this.fireMissile = false
     }
+
+    this.missileGroup.children.each(function (item) {
+      item.y = item.y - 15
+      if (item.y < 0) {
+        item.destroy()
+      }
+    })
   }
 }
 
